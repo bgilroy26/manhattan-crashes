a07c28be6010b80c57a889905200bf330414d507
bgilroy26 <bdgilroy@gmail.com>
2017-09-09 20:13:53 -0400

    created a csv file of all taxi-involved crashes from the last 3 mos

--- starter.py
+++ starter.py
@@ -1,3 +1,6 @@
 import folium
 map = folium.Map(location=[38.58, -99.09], zoom_start=6)
+
+map.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi, I am a Marker", icon=folium.Icon(color='green')))
+
 map.save("Map3.html")
